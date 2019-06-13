cf131693773cd4139a561cbbd32725a8d9a51b2b
saad hafa <saad.811@icloud.com>
2019-06-13 20:19:57 +0000

    added toggle animation with jquery for tweets form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -54,7 +54,7 @@ const data = [
   // loop in to the object and append into the main
   function renderTweets(tweets) {
       tweets.forEach(posts => {
-          let element = createTweetElement(posts);
+          const element = createTweetElement(posts);
         $('main .tweets-container').prepend(element);
       });
   }
@@ -65,24 +65,24 @@ const data = [
   // create html elements
   function createTweetElement(tweet) {
     // tweet content 
-    let name = tweet.user.name;
-    let imgValue = tweet.user.avatars.small;
-    let usename = tweet.user.handle;
-    let content = tweet.content.text;
-    let date = new Date(tweet.created_at);
+    const name = tweet.user.name;
+    const imgValue = tweet.user.avatars.small;
+    const usename = tweet.user.handle;
+    const content = tweet.content.text;
+    const date = new Date(tweet.created_at);
 
     // create main all elements
-    let $tweet = $('<article>');
-    let header = $('<header>');
-    let tweetTextContent = $('<p>').text(content);
-    let tweetFooter = $('<footer>');
-    let footerTextContent = $('<p>').text(date);
-    let imgContainer = $('<div>').addClass('img-tweet-container');
-    let img = $('<img>').attr('src',imgValue);
-    let headerTitleContainer =  $('<div>').addClass('text-header-content');
-    let headerTitleTextContent = $('<h2>').text(name);
-    let avatarContainer = $('<div>').addClass('avatar-name');
-    let avatarTextContent = $('<strong>').text(usename);
+    const $tweet = $('<article>');
+    const header = $('<header>');
+    const tweetTextContent = $('<p>').text(content);
+    const tweetFooter = $('<footer>');
+    const footerTextContent = $('<p>').text(date);
+    const imgContainer = $('<div>').addClass('img-tweet-container');
+    const img = $('<img>').attr('src',imgValue);
+    const headerTitleContainer =  $('<div>').addClass('text-header-content');
+    const headerTitleTextContent = $('<h2>').text(name);
+    const avatarContainer = $('<div>').addClass('avatar-name');
+    const avatarTextContent = $('<strong>').text(usename);
     // append all elements and return value 
    
     imgContainer
@@ -121,24 +121,31 @@ const data = [
       .then(result => renderTweets(result))
     };
 
+    // nav button on click handler
+    $('#compose').on('click', function(){
+      $('.new-tweet').slideToggle(500);
+      const input = document.querySelector('textarea');
+      input.select();
+    })
+
 
 
 
     $('form').on('submit', function(e){
       e.preventDefault();
-      let formData = $(e.target).find('textarea').val()
+      const formData = $(e.target).find('textarea').val();
       if(formData === '' || formData === null){
-        alert('please enter something');
+        $('#showerror').text('please enter something in the form');
         return
       }
       if(formData.length > 140){
-        alert('your tweet is greater then 140')
+        $('#showerror').text('your tweet is greater then 140');
         return
       }else{
           $.ajax({ method: 'POST', url: '/tweets', data:{text:formData}})
             .done(data => loadTweets());
           document.querySelector('form').reset();
-          
+          $('#showerror').text('');
       }
     });
     loadTweets();
