6a9e55c1a1ce4693badd2b7c6b676712a6ef3bdc
saad hafa <saad.811@icloud.com>
2019-06-14 00:14:26 +0000

    removed simulateDelay function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,54 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-
-
   // loop in to the object and append into the main
   function renderTweets(tweets) {
       tweets.forEach(posts => {
@@ -130,10 +82,11 @@ const data = [
 
 
 
-
+    // when form is submited
     $('form').on('submit', function(e){
       e.preventDefault();
       const formData = $(e.target).find('textarea').val();
+      // checking if data is not empty or greater then 140
       if(formData === '' || formData === null){
         $('#showerror').text('please enter something in the form');
         return
@@ -142,6 +95,7 @@ const data = [
         $('#showerror').text('your tweet is greater then 140');
         return
       }else{
+         // post data to db 
           $.ajax({ method: 'POST', url: '/tweets', data:{text:formData}})
             .done(data => loadTweets());
           document.querySelector('form').reset();
