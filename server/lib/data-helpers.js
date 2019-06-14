d02374e59e6f1ed0801f43f8b369464c1753e295
saad hafa <saad.811@icloud.com>
2019-06-14 00:03:09 +0000

    add mongo node  driver and replaced json db with the mongo db its  working fine for now

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,18 +9,20 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+         db.collection('tweets').insertOne(newTweet);
         callback(null, true);
-      });
+        return
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+        // const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+        db.collection("tweets").find().toArray((err, results) => {
+           callback(err,results);
+          return
+          // This is the end..
+        });
+          
     }
 
   };
